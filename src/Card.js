7f5e664203583c443db09ce0fe2268dd2dce0346
maayanadivi <maayana2@ac.sce.ac.il>
2021-08-19 15:14:03 +0300

    41. convert card comonent to ckass base component

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React,{Component} from 'react'
 import './App.css';
 
-const Card = props => {
-    console.log(props)
-    return (
-        <div className="card">
-            <img src={props.avatar} alt="Avatar" style={{ width: '100%' }} />
-            <div className="container">
-                <h4><b>{props.name}</b></h4>
-                <p>{props.title}</p>
-                <input type="text" onChange={props.onChangeName} value={props.name}/>
-                <p><button className="button button-red" onClick={props.onDelete}>Delete</button></p>
-                <div>{props.children}</div>
+class Card extends Component {
+    render(){
+
+        return (
+            <div className="card">
+                <img src={this.props.avatar} alt="Avatar" style={{ width: '100%' }} />
+                <div className="container">
+                    <h4><b>{this.props.name}</b></h4>
+                    <p>{this.props.title}</p>
+                    <input type="text" onChange={this.props.onChangeName} value={this.props.name}/>
+                    <p><button className="button button-red" onClick={this.props.onDelete}>Delete</button></p>
+                    <div>{this.props.children}</div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default Card
