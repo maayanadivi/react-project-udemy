3338dda077caba826353c7c942d296dfaafa81bd
maayanadivi <maayana2@ac.sce.ac.il>
2021-08-25 11:40:23 +0300

    add text box for search

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect} from 'react';
 import './App.css';
 import Card from './Card';
 import { ThemeProvider } from 'styled-components'
-import Button from './element/Button'
 import axios from 'axios'
 const theme = {
   primary: '#4CAF50',
@@ -11,53 +10,38 @@ const theme = {
 
 function App() {
  
-  const [cards, setCards] = useState([])
-  const [showCard, setShowCard] = useState(true)
+  const [card, setCard] = useState([])
+  const [id, setId] = useState(1)
   useEffect(() => {
-    axios.get('http://jsonplaceholder.typicode.com/users')
+    axios.get(`http://jsonplaceholder.typicode.com/users/${id}`)
       .then(res=>{
       console.log(res.data)
-      setCards(res.data)
+      setCard(res.data)
     })
-  }, [])
+  }, [id])
   
-  const deleteCardHandler = (cardIndex) => {
-    const cards_copy = [...cards]
-    cards_copy.splice(cardIndex, 1)
-    console.log('cards_copy', cards_copy)
-    console.log('cards', cards)
-    setCards(cards_copy)
-
-  }
+ 
   const changeNameHandler = (event, id) => {
-    //1. which card
-    const cardIndex = cards.findIndex(card => card.id === id)
-    //2. make a copy of the cards
-    const cards_copy = [...cards]
-    //3. change the name of the specific card
-    cards_copy[cardIndex].name = event.target.value
-    //4. set the cards with the latest version of card copy
-    setCards(cards_copy)
+    //1. make a copy of the cards
+    const card_copy = {...card}
+    //2. change the name of the specific card
+    card_copy.name = event.target.value
+    //3. set the cards with the latest version of card copy
+    setCard(card_copy)
   }
   
-  const classes = ['button']
-  if (cards.length < 3) classes.push('pink')
-  if (cards.length < 2) classes.push('red text');
-  const cardsMarkup = showCard && (
-    cards.map((card, index) => <Card
-      name={card.name}
-      phone={card.phone}
-      key={card.id}
-      onDelete={() => deleteCardHandler(index)}
-      onChangeName={(event) => changeNameHandler(event, card.id)}
-    />)
-  )
+  
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-      
-        {cardsMarkup}
+        <input type="text" value={id} onChange={e=>setId(e.target.value)}/>
+        <Card
+          name={card.name}
+          phone={card.phone}
+          key={card.id}
+          onChangeName={(event) => changeNameHandler(event, card.id)}
+        />
       </div>
     </ThemeProvider>
   );
